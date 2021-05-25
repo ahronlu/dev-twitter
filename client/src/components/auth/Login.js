bf3ea74aed2c6a22fec73f8b8bf6f4a6d21d93eb
koftov <ahronlu@gmail.com>
2021-05-25 20:20:39 +0300

    init

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { ReactComponent as Bird } from '../../Bird.svg';
+import { login } from '../../actions/auth';
+
+const Login = () => {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  if (isAuthenticated) {
+    return <Redirect to="/home" />;
+  }
+
+  return (
+    <div className="Login">
+      <svg viewBox="0 0 24 24" aria-label="Twitter" className="bird r-13gxpu9 r-4qtqp9 r-yyyyoo r-16y2uox r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+      <h1 className="lead bold">
+        Log in to Twitter
+      </h1>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary rounded" value="Login" disabled={formData.email === "" || formData.password === "" }/>
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/signup">Sign up for Twitter</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
