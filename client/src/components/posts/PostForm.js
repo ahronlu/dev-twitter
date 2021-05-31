d0c27f6f627931381c6444c8b6fd4382b028d506
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-31 04:29:28 +0300

    Update PostForm.js

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -9,9 +9,6 @@ const PostForm = () => {
 
   return (
     <div className="post-form">
-      <div className="bg-primary p">
-        <h3>Say Something...</h3>
-      </div>
       <form
         className="form my-1"
         onSubmit={(e) => {
@@ -24,12 +21,12 @@ const PostForm = () => {
           name="text"
           cols="30"
           rows="5"
-          placeholder="Create a post"
+          placeholder="What's happening?"
           value={text}
           onChange={(e) => setText(e.target.value)}
           required
         />
-        <input type="submit" className="btn btn-dark my-1" value="Submit" />
+        <input type="submit" className="btn btn-primary my-1" value="Tweet" disabled={!text} />
       </form>
     </div>
   );
