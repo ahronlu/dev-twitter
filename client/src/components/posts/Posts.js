837ecaec2a02978b6c730222c6a8eb269bd581f0
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-31 04:40:56 +0300

    Update Posts.js

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -15,10 +15,7 @@ const Posts = () => {
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Posts</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Welcome to the community
-      </p>
+      <h1 className="large text-primary">Home</h1>
       <PostForm />
       <div className="posts">
         {posts.map((post) => (
